92c6c4a977279655a39f8d71fb1614b63dd094ae
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-08 17:00:14 -0300

    minor changes

--- init.py
+++ init.py
@@ -1,1 +1,18 @@
-return("Hello world")
+from flask import Flask
+from flask_cors import CORS
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+CORS(app)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hello World API!<br/>")
