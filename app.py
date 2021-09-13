f5494ac8270fa0dbd4e97473c91dcdc446c2c36a
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-13 15:19:17 -0300

    lxml

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ if __name__ == '__main__':
 def fun():
 	r = requests.get('https://www.vea.com.ar/milanesa-nalga-5/p')
 	soup = BeautifulSoup(r.content, 'html.parser')	
-	return soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";"
+	a=str(soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";")
+	return "holi"
