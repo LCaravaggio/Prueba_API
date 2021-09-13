163e6f3e3594360eb31da349704a0ed1f5e30b19
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-13 14:44:29 -0300

    vea

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ if __name__ == '__main__':
 
 def fun():
 	r = requests.get('https://www.vea.com.ar/milanesa-nalga-5/p')
-	return r.text
+	soup = BeautifulSoup(r.content, 'html.parser')
+	return soup
