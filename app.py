53b49efede7612b6a76dc1bdcd1e33d93b596e60
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 12:12:39 -0300

    url

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
     site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
     r = requests.get(site)
     soup = BeautifulSoup(r.content, 'html.parser')
-    return soup.find_all('span', {'class':'vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price'})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";" 
+    return soup.span
 
 
 if __name__ == "__main__":
