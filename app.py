130fd5b1da6741acf87c77e1d4c61ae9aff5bcb0
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 14:57:24 -0300

    url

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def scrap():
     b=""
 
     soup = BeautifulSoup(r.content, 'html.parser')
-    #b += soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text + ";"
-    #b += soup.find_all('span', {'class':'vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price'})[0].text + "," 
-    #b += soup.find_all('span', {'class':'vtex-product-price-1-x-currencyFraction vtex-product-price-1-x-currencyFraction--shelf-main-selling-price'})[0].text 
-    return soup.find("span", {"class": "vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price"}).text
+    b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price"}).text
+    b+=";"
+    b+=soup.find("span", {"class": "vtex-store-components-3-x-productBrand "}).text
+    b+=","
+    b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyFraction vtex-product-price-1-x-currencyFraction--shelf-main-selling-price"}).text    
+    return b
