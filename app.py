1cf6e4308cfe19284f5c11145672ef8ef3b47dfe
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 11:48:34 -0300

    url

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 from flask_cors import cross_origin
-
 from pretty_html_table import build_table
 from flask import Flask, render_template, request
 
 import pandas as pd
-from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
 app = Flask(__name__)
@@ -18,7 +16,7 @@ def index():
         site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
 	r = requests.get(site)
 	soup = BeautifulSoup(r.content, 'html.parser')	
-        return soup.span
+        return soup
     else:
         return "get"
 
