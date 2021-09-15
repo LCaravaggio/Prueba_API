afe0f1f78e64dfa6a339df2bac9d01e84f49cd0c
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 11:36:04 -0300

    url

--- app.py
+++ app.py
@@ -61,5 +61,6 @@ def fun():
 	web_request = Request(site, headers={'User-Agent':'Mozilla/5.0'})
 
 	web_page = urlopen(web_request).read()
-	soup = BeautifulSoup(web_page, 'html.parser')
-	return soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";"
+	#soup = BeautifulSoup(web_page, 'html.parser')
+	#return soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";"
+	return "hola"
