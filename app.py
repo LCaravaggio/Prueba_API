4661b3501047fe2a6223c03f83c011d26d148b65
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 12:03:29 -0300

    url

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 @cross_origin()
 def index():
-    if request.method == 'POST':
-	site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
-		r = requests.get(site)
-		soup = BeautifulSoup(r.content, 'html.parser')	
-		return soup
-    else:
-        return "get"
+  if request.method == 'POST':
+    #site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
+    #r = requests.get(site)
+    #soup = BeautifulSoup(r.content, 'html.parser')
+    return "soup"
+  else:
+    return "get"
 
 
 if __name__ == "__main__":
