1c67d8989922e3667e8d6f70646f8a8c3e329085
LCaravaggio <lcaravaggio_mecon@SERVIDORES.MECON.AR>
2021-09-15 11:42:10 -0300

    url

--- app.py
+++ app.py
@@ -1,66 +1,23 @@
-# app.py
-from flask import Flask, request, jsonify
-
-from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from flask_cors import cross_origin
+from supp import company_financials
+from pretty_html_table import build_table
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
 
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
+@cross_origin()
 def index():
-    #return "<h1>Welcome to our server !!</h1>"
-	return fun()
+    if request.method == 'POST':
+        site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
+	r = requests.get(site)
+	soup = BeautifulSoup(r.content, 'html.parser')	
+        return soup.span
+    else:
+        return "get"
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
 
-
-def fun():
-	site = 'https://www.vea.com.ar/bife-de-chorizo-2/p'
-	web_request = Request(site, headers={'User-Agent':'Mozilla/5.0'})
-
-	web_page = urlopen(web_request).read()
-	#soup = BeautifulSoup(web_page, 'html.parser')
-	#return soup.find_all('span', {'class':'vtex-store-components-3-x-productBrand '})[0].text.replace(" ","").replace("\n","").replace("\r","") + ";"
-	return "hola"
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
