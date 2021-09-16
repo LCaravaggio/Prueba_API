f090ee56695b9132b2673ccc9fad4204e8bca7f1
unknown <caravaggio@gmail.com>
2021-09-15 22:30:55 -0300

    try

--- app.py
+++ app.py
@@ -45,14 +45,17 @@ def archivo(b):
 def scrap(site):
     r = requests.get(site)
     b=""
-
-    soup = BeautifulSoup(r.content, 'html.parser')
-    b+=soup.find("span", {"class": "vtex-breadcrumb-1-x-term vtex-breadcrumb-1-x-term--breadcrumb-style ph2 c-on-base"}).text    
-    b+=";"
-    b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price"}).text
-    b+=","
-    b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyFraction vtex-product-price-1-x-currencyFraction--shelf-main-selling-price"}).text    
-    b+="\n"
+    
+    try:
+        soup = BeautifulSoup(r.content, 'html.parser')
+        b+=soup.find("span", {"class": "vtex-breadcrumb-1-x-term vtex-breadcrumb-1-x-term--breadcrumb-style ph2 c-on-base"}).text    
+            b+=";"
+        b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyInteger vtex-product-price-1-x-currencyInteger--shelf-main-selling-price"}).text
+        b+=","
+        b+=soup.find("span", {"class": "vtex-product-price-1-x-currencyFraction vtex-product-price-1-x-currencyFraction--shelf-main-selling-price"}).text    
+        b+="\n"
+    except:
+        _
     
     return b
 
